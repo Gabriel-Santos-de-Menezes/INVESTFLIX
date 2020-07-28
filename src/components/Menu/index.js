5b625f7510473a64188b30b9b11279f037e51af4
Gabriel Menezes <gabriel.santos.m112@gmail.com>
2020-07-27 23:40:47 -0300

    Setup inicial do Projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Logo from '../../assets/img/gaflix.png'
+import './Menu.css'
+import Button from '../Button'
+//import ButtonLink from './components/ButtonLink';
+
+function Menu(){
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="gaflix Logo"/>
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídio
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
